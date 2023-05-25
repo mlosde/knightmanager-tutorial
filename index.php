293861ee85fa5dcd3701804795b02cb9a2f6e54a
Manuel Osswald <mlos4lp@gmail.com>
2023-05-25 13:53:29 +0200

    update index.php with link to README

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="en-US">
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
@@ -15,7 +15,7 @@
   <p>English Version: <a href="/en.html">Go to English Version</a> (in progress, last change: <?php echo date('Y-m-d', filemtime('en.html')); ?>)</p>
   <h1>Contribute</h1>
   <p>This tutorial can be expanded and kept up to date on GitHub.</p>
-  <p>If you're interested in contributing to the tutorial, please visit the following link on GitHub:</p>
-  <a href="https://github.com/mlosde/knightmanager-tutorial">GitHub Tutorial Repository</a>
+  <p>If you're interested in contributing to the tutorial, please visit the following link for further instructions:</p>
+  <a href="https://github.com/mlosde/knightmanager-tutorial/blob/main/README.md">Instructions for contribution on GitHub Repository</a>
 </body>
 </html>
